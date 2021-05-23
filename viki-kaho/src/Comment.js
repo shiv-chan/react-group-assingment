008033c4d758b8002b80ef188ea2ad4e11046c52
shiv-chan <kshibuya31@gmail.com>
2021-05-23 12:39:45 +0900

    set EditableaFormRow function
    Fix edit button defect

--- viki-kaho/src/Comment.js
+++ viki-kaho/src/Comment.js
@@ -1,49 +1,107 @@
 import React from 'react';
 
 class Comment extends React.Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
 		this.state = {
-			editComment: false,
-			name : this.props.name,
+			isEditing: false,
+			name: this.props.name,
 			email: this.props.email,
-			body: this.props.body
-		}
+			body: this.props.body,
+		};
 	}
 
+	handleEdit = () => {
+		this.setState({
+			isEditing: true,
+		});
+	};
 
 	handleSave = (e) => {
 		e.preventDefault();
 		this.setState({
-			editComment: false
+			isEditing: false,
 		});
-	}
+	};
 
-	render(){
-		return (
-			this.state.editComment ? 
-				<tr>
-					<td className="hidden"><form id={`form${this.props.id}`} onSubmit={this.handleSave}/></td>
-					<td><input form={`form${this.props.id}`} type="text" name="name" value={this.state.name} onChange={(e)=>this.setState({name: e.target.value})}/></td>
-					<td><input form={`form${this.props.id}`} type="email" name="email" value={this.state.email} onChange={(e)=>this.setState({email: e.target.value})}/></td>
-					<td><input form={`form${this.props.id}`} type="text" name="body" value={this.state.body} onChange={(e)=>this.setState({body: e.target.value})}/></td>
-					<td className="buttons"><button className="save" form={`form${this.props.id}`} type="submit">Save</button> </td>
-				</tr> 
-			:
-				<tr>
-					<td/>
-					<td>{this.state.name}</td>
-					<td>{this.state.email}</td>
-					<td>{this.state.body}</td>
-					<td>
-						<button onClick={() => this.setState({editComment: true})}>Edit</button>
-						<button onClick={() => this.props.handleDelete(this.props.id)}>Delete</button>
-					</td>
-				</tr>
+	render() {
+		return this.state.isEditing ? (
+			<EditableFormRow
+				handleChangeEmail={(e) => this.setState({ email: e.target.value })}
+				handleChangeName={(e) => this.setState({ name: e.target.value })}
+				handleChangeBody={(e) => this.setState({ body: e.target.value })}
+				name={this.state.name}
+				email={this.state.email}
+				body={this.state.body}
+				id={this.props.id}
+				handleSave={this.handleSave}
+			/>
+		) : (
+			<tr>
+				<td />
+				<td>{this.state.name}</td>
+				<td>{this.state.email}</td>
+				<td>{this.state.body}</td>
+				<td>
+					<button onClick={this.handleEdit}>Edit</button>
+					<button onClick={() => this.props.handleDelete(this.props.id)}>
+						Delete
+					</button>
+				</td>
+			</tr>
 		);
 	}
 }
 
 export default Comment;
 
-
+function EditableFormRow({
+	handleChangeName,
+	handleChangeEmail,
+	handleChangeBody,
+	name,
+	email,
+	body,
+	id,
+	handleSave,
+}) {
+	return (
+		<tr>
+			<td className="hidden">
+				<form id={`form${id}`} onSubmit={handleSave} />
+			</td>
+			<td>
+				<input
+					form={`form${id}`}
+					type="text"
+					name="name"
+					value={name}
+					onChange={handleChangeName}
+				/>
+			</td>
+			<td>
+				<input
+					form={`form${id}`}
+					type="email"
+					name="email"
+					value={email}
+					onChange={handleChangeEmail}
+				/>
+			</td>
+			<td>
+				<input
+					form={`form${id}`}
+					type="text"
+					name="body"
+					value={body}
+					onChange={handleChangeBody}
+				/>
+			</td>
+			<td className="buttons">
+				<button className="save" form={`form${id}`} type="submit">
+					Save
+				</button>
+			</td>
+		</tr>
+	);
+}
